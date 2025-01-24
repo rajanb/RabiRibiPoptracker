b2b35f2f34fab674a78b6be839ce1e2fd9135a43
Rajan Bhatoy <74066873+rajanb@users.noreply.github.com>
2025-01-24 21:43:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rabi Ribi AP PopTracker
+
+This a PopTracker pack for Rabi Ribi for use with Archipelago
+
+For more information visit archipelago.gg
+
+## Installation
+
+Just download the latest build or source and put in your packs folder (unpacked)
---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,27 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type override. The third value is an optional increment multiplier for consumables. (feel free to expand the table with any other values you might need (i.e. special initial values, etc.)!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+BASE_ITEM_ID = 8350438193300
+ITEM_MAPPING = {
+	[BASE_ITEM_ID + 0x01] = { {"airdash"} },
+	[BASE_ITEM_ID + 0x02] = { {"airjump"} },
+	[BASE_ITEM_ID + 0x05] = { {"whirl"} },
+	[BASE_ITEM_ID + 0x06] = { {"bomb"} },
+	[BASE_ITEM_ID + 0x09] = { {"fire"} },
+	[BASE_ITEM_ID + 0x0A] = { {"roll"} },
+	[BASE_ITEM_ID + 0x0D] = { {"light"} },
+	[BASE_ITEM_ID + 0x10] = { {"hammer"} },
+	[BASE_ITEM_ID + 0x13] = { {"slippers"} },
+	[BASE_ITEM_ID + 0x14] = { {"slide"} },
+	[BASE_ITEM_ID + 0x18] = { {"walljump"} },
+	[BASE_ITEM_ID + 0x19] = { {"water"} },
+
+	[BASE_ITEM_ID + 0x3E] = { {"atk"} },
+	[BASE_ITEM_ID + 0x3F] = { {"egg"} },
+	[BASE_ITEM_ID + 0x41] = { {"hp"} },
+	[BASE_ITEM_ID + 0x42] = { {"mp"} },
+	[BASE_ITEM_ID + 0x44] = { {"pack"} },
+	[BASE_ITEM_ID + 0x45] = { {"regen"} },
+
+
+}
